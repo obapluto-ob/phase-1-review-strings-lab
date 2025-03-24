ef7e22f7acde79cca11e38f2f948e0cf5216df15
root <root@PLUTOMANIA.localdomain>
2025-03-24 09:40:08 +0300

    Completed string manipulation lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Your Name'; // Replace 'Your Name' with any name
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
